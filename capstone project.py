fd9902718813b5d4b994b5b79606dd91136fe944
Surbhi Sarawagi <surbhisarawagi1996@gmail.com>
2019-07-04 18:55:34 +0530

    Add files via upload

---
+++ capstone project.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ##    Coursera datascience Capstone Assignment
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print ('hello capstone project')
+
+
+# In[ ]:
+
+
+
+
